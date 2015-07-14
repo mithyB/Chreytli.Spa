b11af17c6cfaad70ce2ca2b8c75fcd35bdce8701
Michael <michael.buholzer@outlook.com>
2015-07-14 19:22:28 +0200

    added polls

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 ﻿(function (moduleId, valueId, ng) {
     'use strict';
     
-    var baseUrl = 'http://chreytli.noip.me/Chreytli.Api/';
-    //var baseUrl = 'http://localhost:53994/';
+    //var baseUrl = 'http://chreytli.noip.me/Chreytli.Api/';
+    var baseUrl = 'http://localhost:53994/';
     var apiUrl = baseUrl + 'api/';
     var tokenUrl = baseUrl + 'token';
     var metaUrl = apiUrl + 'Meta/';
@@ -11,7 +11,10 @@
         baseUrl: baseUrl,
         apiUrl: apiUrl,
         tokenUrl: tokenUrl,
-        metaUrl: metaUrl
+        metaUrl: metaUrl,
+
+        postsPageSize: 4,
+        pollsPageSize: 4
     };
 
     ng.module(moduleId).value(valueId, value);
--- controllers/poll.js
+++ controllers/poll.js
@@ -2,14 +2,139 @@
     'use strict';
 
     ng.module(moduleId).controller(controllerId, [
-        '$scope',
+        '$resource',
+        'globalConfig',
+        'accountService',
         controller
     ]);
 
-    function controller($scope) {
+    function controller($resource, globalConfig, accountService) {
         var vm = this;
 
-        
+        vm.polls = [];
+
+        vm.page = 0;
+
+        var Poll = $resource(globalConfig.apiUrl + 'Polls/:id', {}, {
+            query: { method: 'GET', isArray: true },
+            create: { method: 'POST', headers: { authorization: localStorage.getItem('access_token') } },
+            delete: { method: 'DELETE', params: { id: '@id' }, headers: { authorization: localStorage.getItem('access_token') } },
+            vote: {
+                method: 'POST', url: globalConfig.apiUrl + 'Polls/:id/Vote', params: { id: '@id' },
+                headers: { authorization: localStorage.getItem('access_token') }
+            }
+        });
+
+        accountService.onAccountLoaded(function (account) {
+            var accountId = account ? account.id : '';
+            Poll.query({ userId: accountId, page: vm.page, pageSize: globalConfig.pollsPageSize }).$promise.then(function (result) {
+                loadData(result, accountId);
+            }, function (error) {
+                console.error(error);
+            });
+        });
+
+        vm.loadMore = function (dataLoaded) {
+            vm.page++;
+            var account = accountService.getAccount();
+            var accountId = account ? account.id : '';
+            Poll.query({ userId: accountId, page: vm.page, pageSize: globalConfig.pollsPageSize }).$promise.then(function (result) {
+                loadData(result, accountId);
+
+                if (dataLoaded) { dataLoaded(result); }
+
+            }, function (error) {
+                console.error(error);
+            });
+        };
+
+        function loadData(result, accountId) {
+            vm.isMoreDataAvailable = result.length === globalConfig.pollsPageSize;
+            ng.forEach(result, function (x) {
+                x.date = moment(moment.utc(x.date).toDate()); // utc to local
+                setPercentages(x);
+                x.showResult = x.isVoted || !accountId;
+                vm.polls.push(x);
+            });
+        }
+
+        function setPercentages(poll) {
+            ng.forEach(poll.choices, function (x) {
+                x.percentage = (x.votes / poll.totalVotes * 100) + '%';
+            });
+        }
+
+        vm.toggleVote = function (poll, choice) {
+            if (!poll.choiceIds || !poll.multipleChoice) {
+                poll.choiceIds = [];
+            }
+
+            if (poll.choiceIds.indexOf(choice.id) > -1) {
+                poll.choiceIds.splice(poll.choiceIds.indexOf(choice.id), 1);
+            } else {
+                poll.choiceIds.push(choice.id);
+            }
+        };
+
+        vm.vote = function (poll) {
+            Poll.vote({
+                id: poll.id,
+                userId: accountService.getAccount().id
+            }, poll.choiceIds).$promise.then(function (result) {
+                result.showResult = true;
+                setPercentages(result);
+
+                result.author = poll.author;
+                result.date = poll.date;
+
+                ng.copy(result, poll);
+            }, function (error) {
+                console.error(error);
+            });
+        };
+
+        vm.createPoll = function () {
+            if (vm.newPoll) {
+                vm.newPoll.date = moment();
+                vm.newPoll.authorId = accountService.getAccount().id;
+                Poll.create(vm.newPoll).$promise.then(function (result) {
+                    result.author = {
+                        username: accountService.getAccount().username
+                    };
+                    result.date = moment(moment.utc(result.date).toDate());
+                    vm.polls.splice(0, 0, result);
+                }, function (error) {
+                    console.log(error);
+                });
+            }
+
+            $('#newPollModal').modal('hide');
+
+            delete vm.newPoll;
+        };
+
+        vm.addChoice = function () {
+            if (!vm.newPoll) {
+                vm.newPoll = {};
+            }
+            if (!vm.newPoll.choices) {
+                vm.newPoll.choices = [];
+            }
+            vm.newPoll.choices.push(ng.copy(vm.newChoice));
+            delete vm.newChoice;
+        };
+
+        vm.removeChoice = function (choice) {
+            var index = vm.newPoll.choices.indexOf(choice);
+            vm.newPoll.choices.splice(index, 1);
+        };
+
+        vm.createPollValid = function () {
+            return (vm.newPoll &&
+                    vm.newPoll.title &&
+                    vm.newPoll.choices &&
+                    vm.newPoll.choices.length > 0);
+        };
     }
 
 })(appName, 'poll', angular);
